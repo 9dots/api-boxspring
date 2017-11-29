d07098e5a0297e9b439e0223666346cf49a88567
Connor Borden <connorbo97@gmail.com>
2017-11-29 00:00:11 -0800

    Added comments, implemented /add request

--- app.js
+++ app.js
@@ -3,24 +3,34 @@ const init = require('lib/init')
 
 
 const app = express();
+
+//all projects stored under {current directory}/projects/{UNIQUE NAME HASH}
 const DIR = __dirname + '/projects'
 
-app.post('/init', (req, res) => {
-  let {code, output, name} = yield init(DIR)
+app.post("/init", function * (req, res) {
+  let { code, output, name } = yield init(DIR);
   if (code === 0) {
-    return {ok: true, name}
+    return { ok: true, name };
   } else {
-    return {ok: false, error: output}
+    return { ok: false, error: output };
   }
-})
 
+  res.json({name});                                           //FIX: most likely not the proper way to
+});
+
+// download :name from cloud storage
+// yarn add :package
+// bundle
+// upload dir to cloud storage
+// upload bundle to cloud storage
+// reutnr {ok: bool, buildUrl: url, tag: string}
 app.post('/add', (req, res) => {
-  // download :name from cloud storage
-  // yarn add :package
-  // bundle
-  // upload dir to cloud storage
-  // upload bundle to cloud storage
-  // reutnr {ok: bool, buildUrl: url, tag: string}
+  let {bundleName, error, output} = yield add(DIR, req.body.name, req.body.pkg);            //FIX: Figure out where you're getting the project name from, as well as the package name
+  if (error) {
+    return {ok:false, error:output}
+  } else {
+    return {ok:true, buildUrl: DIR + '/' + req.params.name + '/' + bundleName, tag: 'FIX LATER'}      //FIX: not sure what the tag field is, nor if that's the correct buildURL
+  }
 })
 
 app.post('/upgrade', (req, res) => {
