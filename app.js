73e3d4d1bc4888608e49ed4590aa2f84692c9e86
Connor Borden <connorbo97@gmail.com>
2017-12-01 11:54:04 -0800

    Starting switch to async await

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const express = require('express');
-const init = require('lib/init')
+const init = require('./lib/init');
+const add = require('./lib/add');
+const upgrade = require('./lib/upgrade');
+const remove = require('./lib/remove');
 
 
 const app = express();
 
 //all projects stored under {current directory}/projects/{UNIQUE NAME HASH}
 const DIR = __dirname + '/projects'
-
-app.post("/init", function * (req, res) {
-  let { code, output, name } = yield init(DIR);
+app.post("/init", async function (req, res) {
+  console.log("Init received")
+  let { code, output, name } = await init(DIR);
+  // const name = 'test'
+  // const code = 0
+  // const output = ''
   if (code === 0) {
-    return { ok: true, name };
+    return res.status(200).send({ok: true, name})
   } else {
-    return { ok: false, error: output };
+    return res.status(500).send({ ok: false, error: output });
   }
 
-  res.json({name});                                           //FIX: most likely not the proper way to
 });
 
 // download :name from cloud storage
@@ -23,40 +28,54 @@ app.post("/init", function * (req, res) {
 // bundle
 // upload dir to cloud storage
 // upload bundle to cloud storage
-// reutnr {ok: bool, buildUrl: url, tag: string}
-app.post('/add', (req, res) => {
-  let {bundleName, error, output} = yield add(DIR, req.body.name, req.body.pkg);            //FIX: Figure out where you're getting the project name from, as well as the package name
+// return {ok: bool, buildUrl: url, tag: string}
+app.post('/add', function * (req, res) {
+  let {bundleName, error, output} = yield add(DIR, req.body.name, req.body.pkg);            
   if (error) {
     return {ok:false, error:output}
   } else {
-    return {ok:true, buildUrl: DIR + '/' + req.params.name + '/' + bundleName, tag: 'FIX LATER'}      //FIX: not sure what the tag field is, nor if that's the correct buildURL
+    return {ok:true, buildUrl: DIR + '/' + req.body.name + '/' + bundleName, tag: 'FIX LATER'} 
   }
 })
 
-app.post('/upgrade', (req, res) => {
-  // download :name from cloud storage
-  // yarn upgrade :package
-  // bundle
-  // upload dir to cloud storage
-  // upload bundle to cloud storage
-  // reutnr {ok: bool, buildUrl: url, tag: string}
+// download :name from cloud storage
+// yarn upgrade :package
+// bundle
+// upload dir to cloud storage
+// upload bundle to cloud storage
+// return {ok: bool, buildUrl: url, tag: string}
+app.post('/upgrade', function * (req, res) {
+  let {bundleName, error, output} = yield upgrade(DIR, req.body.name, req.body.pkg);           
+  if (error) {
+    return {ok:false, error:output}
+  } else {
+    return {ok:true, buildUrl: DIR + '/' + req.body.name + '/' + bundleName, tag: 'FIX LATER'}
+  }
 })
 
-app.post('/remove', (req, res) => {
-  // download :name from cloud storage
-  // yarn remove :package
-  // bundle
-  // upload dir to cloud storage
-  // upload bundle to cloud storage
-  // reutnr {ok: bool, buildUrl: url, tag: string}
+// download :name from cloud storage
+// yarn remove :package
+// bundle
+// upload dir to cloud storage
+// upload bundle to cloud storage
+// return {ok: bool, buildUrl: url, tag: string}
+app.post('/remove', function * (req, res) {
+  let {bundleName, error, output} = yield remove(DIR, req.body.name, req.body.pkg);            
+  if (error) {
+    return {ok:false, error:output}
+  } else {
+    return {ok:true, buildUrl: DIR + '/' + req.body.name + '/' + bundleName, tag: 'FIX LATER'}
+  }
 })
 
-app.post('/dependencies', (req, res) => {
-  // download :name/package.json from cloud storage
-  // return {ok: bool, dependencies: array}
+// download :name/package.json from cloud storage
+// return {ok: bool, dependencies: array}
+app.post('/dependencies', function * (req, res) {
+  //FIX: Figure out where you're getting the project name from, as well as the package name
+  //let {bundleName, error, output} = yield dependencies(DIR, req.body.name);            
 })
 
-
+  
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
